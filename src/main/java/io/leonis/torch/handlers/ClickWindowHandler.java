6018d9851e2c8ab264ea4a7ba1dfe85de156d665
Jeroen de Jong <thumbnail95@gmail.com>
2017-10-25 16:32:34 +0200

    Remove failed abstraction

--- src/main/java/io/leonis/torch/handlers/ClickWindowHandler.java
+++ src/main/java/io/leonis/torch/handlers/ClickWindowHandler.java
@@ -7,23 +7,18 @@ import java.util.function.Function;
 import lombok.AllArgsConstructor;
 
 /**
- * The Class WindowClickHandler
+ * The Class ClickWindowHandler
  *
  * Sets the {@link Window} on which an {@link MouseActionType#CLICK_DOWN} is fired as the active
  * {@link Window}.
  *
  * @author Jeroen de Jong
  */
-@AllArgsConstructor
-public final class WindowClickHandler implements MouseListener {
-
-  private final Function<Window, ?> handler;
-
+public final class ClickWindowHandler implements MouseListener {
   @Override
   public boolean onUnhandledKeyStroke(final WindowBasedTextGUI gui, final MouseAction action) {
     return action.getActionType().equals(MouseActionType.CLICK_DOWN)
-        && new ActiveWindowSelector().apply(gui, action.getPosition())
-        .map(handler)
+        && new ActiveWindowSelector().apply(gui, action.getPosition()).map(gui::setActiveWindow)
         .isPresent();
   }
 }
