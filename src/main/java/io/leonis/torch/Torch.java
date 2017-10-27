8034b86a6fb7b7b00237353e9c948d3aec12e381
Jeroen de Jong <thumbnail95@gmail.com>
2017-10-27 14:29:36 +0200

    Remove example code

--- src/main/java/io/leonis/torch/Torch.java
+++ src/main/java/io/leonis/torch/Torch.java
@@ -1,19 +1,12 @@
 package io.leonis.torch;
 
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.screen.*;
 import com.googlecode.lanterna.terminal.*;
-import io.leonis.torch.component.TextBackground;
-import io.leonis.torch.component.graph.*;
 import io.leonis.torch.handlers.*;
-import io.leonis.torch.window.RxWindow;
-import java.awt.Color;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
-import reactor.core.publisher.Flux;
 
 /**
  * The Class Torch.
@@ -23,26 +16,8 @@ import reactor.core.publisher.Flux;
 @AllArgsConstructor
 public final class Torch implements Runnable {
 
-  private final Consumer<WindowBasedTextGUI> drawUnit;
+  private final Consumer<WindowBasedTextGUI> drawFunction;
   private final Component background;
-
-  // TODO Remove. Currently here for debugging LineGraph
-  public static void main(final String[] args) throws IOException {
-    new Thread(new Torch(
-        (gui) -> {
-          gui.addWindow(new RxWindow("Wat", Flux.interval(Duration.ofMillis(100))
-              .map(d -> Math.sin(d * 2 * Math.PI / 20d))
-              .buffer(50, 1)
-              .map(data -> new LineGraph(LineType.THIN, new Gradient(Color.RED, Color.BLUE),
-                  data))));
-          gui.addWindow(new BasicWindow("Test 1"));
-          gui.addWindow(new BasicWindow("Test 2"));
-          gui.addWindow(new BasicWindow("Test 3"));
-          gui.addWindowAndWait(new BasicWindow("Test 4"));
-        },
-        new TextBackground("Background text", TextColor.ANSI.BLUE)
-    )).start();
-  }
 
   @Override
   public void run() {
@@ -59,9 +34,9 @@ public final class Torch implements Runnable {
       gui.addListener(new CycleWindowHandler());
       gui.addListener(new ClickWindowHandler());
       gui.addListener(new StartDragHandler());
-      this.drawUnit.accept(gui);
+      this.drawFunction.accept(gui);
     } catch (final IOException ioe) {
-      throw new RuntimeException("Couldn't start " + ioe);
+      throw new RuntimeException(ioe);
     }
   }
 }
