7c3314f4cd23e3d85505f7034dedf4cdf37c7174
Thomas Hakkers <ThomasHakkers@hotmail.com>
2017-10-31 22:29:51 +0100

    :art: Refactor TextBackground

--- src/main/java/io/leonis/torch/component/TextBackground.java
+++ src/main/java/io/leonis/torch/component/TextBackground.java
@@ -3,9 +3,7 @@ package io.leonis.torch.component;
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.gui2.*;
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 import lombok.*;
 
@@ -39,23 +37,10 @@ public final class TextBackground extends AbstractComponent<TextBackground> {
 
       @Override
       public void drawComponent(final TextGUIGraphics graphics, final TextBackground component) {
-        // TODO this needs some cleanup
         graphics.applyThemeStyle(component.getThemeDefinition().getNormal());
         graphics.setBackgroundColor(component.getColor());
         graphics.fill(' ');
-        final int height = (int)component.getText().chars().filter(i -> i == 10 /* 10 happens to be '\n'*/).count() + 1;
-        final int width = Arrays.asList(component.getText().split("\n")).stream()
-            .map(String::length)
-            .mapToInt(i -> i).max().orElse(0);
-        TerminalSize size = component.getParent().getSize();
-        final int x = Math.max(size.getColumns()/2 - height/2,0);
-        final int y = Math.max(size.getRows()/2 - width/2, 0);
-        final String txt = new String(component.getText())
-            .replace("\n", "\n" + new String(new char[x]).replace("\0", " "));
-        final Label text = new Label(
-            new String(new char[y]).replace("\0", "\n") +
-                new String(new char[x]).replace("\0", " ") +
-                txt);
+        final Label text = new Label(centerText(component.getParent().getSize(), component.getText()));
         text.setBackgroundColor(component.getColor());
         final Color brighterColor = component.getColor().toColor().brighter();
         text.setForegroundColor(
@@ -64,5 +49,28 @@ public final class TextBackground extends AbstractComponent<TextBackground> {
         text.draw(graphics);
       }
     };
+  }
+
+  /**
+   * Centers the given text by adding spaces and new lines based on the given size
+   * @param size Size that the text needs to adjust to
+   * @param text The text to be centered
+   * @return Centered text based on the given size
+   */
+  private String centerText(TerminalSize size, String text) {
+    // height = amount of '\n' + 1
+    final int height = (int)text.chars().filter(i -> i == 10 /* 10 happens to be '\n'*/).count() + 1;
+    // width = length of largest line
+    final int width = Arrays.asList(text.split("\n")).stream()
+        .map(String::length)
+        .mapToInt(i -> i).max().orElse(0);
+    final int x = Math.max(size.getColumns()/2 - height/2,0);
+    final int y = Math.max(size.getRows()/2 - width/2, 0);
+    // Move all lines x spaces to the right
+    return // Move text y lines down
+            new String(new char[y]).replace("\0", "\n") +
+            // Move top line x spaces to the right
+            new String(new char[x]).replace("\0", " ") +
+        text.replace("\n", "\n" + new String(new char[x]).replace("\0", " "));
   }
 }
