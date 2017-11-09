2fa7ae5697905865b2f53098f09af67b3cdbd85f
Jeroen de Jong <thumbnail95@gmail.com>
2017-11-09 20:05:26 +0100

    Make CycleWindowHandler responsible for only one direction

--- src/main/java/io/leonis/torch/handlers/CycleWindowHandler.java
+++ src/main/java/io/leonis/torch/handlers/CycleWindowHandler.java
@@ -2,21 +2,26 @@ package io.leonis.torch.handlers;
 
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.input.*;
+import lombok.AllArgsConstructor;
 
 /**
- * The Class CycleWindowHandler
+ * The Class CycleWindowHandler.
  *
- * Contains logic to cycle the active window by pressing {@code [} or {@code ]}
+ * Contains logic to cycle the active window by pressing a provided {@link Character}. If reverse is
+ * {@code true} the windows will cycle in reverse order.
+ *
  *
  * @author Jeroen de Jong
  */
+@AllArgsConstructor
 public final class CycleWindowHandler implements MultiWindowListener {
+  private final Character character;
+  private final boolean reverse;
 
   @Override
   public boolean onUnhandledKeyStroke(final WindowBasedTextGUI textGUI, final KeyStroke keyStroke) {
-    if (keyStroke.getKeyType().equals(KeyType.Character) &&
-        "[]".contains(keyStroke.getCharacter().toString())) {
-      textGUI.cycleActiveWindow(keyStroke.getCharacter().equals('['));
+    if (keyStroke.getCharacter().equals(this.character)) {
+      textGUI.cycleActiveWindow(this.reverse);
       return true;
     }
     return false;
